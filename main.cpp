51b129c33daa63e21b1da5ea6738df4b6a0c9209
sidhumeet0018 <97530835+sidhumeet0018@users.noreply.github.com>
2022-01-11 19:35:19 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#inclue<iostream>
+using main std ;
+void main()
+{
+cout<<"- 👋 Hi, I’m @sidhumeet0018";
+cout<<"- 👀 I’m interested in ... \n";
+cout<<"- 🌱 I’m currently learning ... \n";
+cout<<"- 💞️ I’m looking to collaborate on ... \n";
+cout<<"- 📫 How to reach me ... ";
+
+<!---
+sidhumeet0018/sidhumeet0018 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+}
